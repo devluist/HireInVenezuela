25b4f0a10efd5e0284bdff62c9ba9895fb5b321d
desLT <des.luistena@hotmail.com>
2016-01-18 17:02:03 -0430

    Primera vez en la web - Parte 9

--- wsgi/hv/hv/settings.py
+++ wsgi/hv/hv/settings.py
@@ -110,12 +110,12 @@ USE_TZ = True
 STATIC_URL = '/multimedia/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'Multimedia/')
 # Additional locations of static files
-# STATICFILES_DIRS = (
-	## Put strings here, like "/home/html/static" or "C:/www/django/static".
-	## Always use forward slashes, even on Windows.
-	## Don't forget to use absolute paths, not relative paths.
-	# os.path.join(WSGI_DIR, 'Multimedia/'),  # '~/hv',
-# )
+STATICFILES_DIRS = (
+	# Put strings here, like "/home/html/static" or "C:/www/django/static".
+	# Always use forward slashes, even on Windows.
+	# Don't forget to use absolute paths, not relative paths.
+	os.path.join(WSGI_DIR, 'Multimedia/'),  # '~/hv',
+)
 
 MEDIA_URL = '/media-url/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'Subidas/')  # '~/hv/Multimedia/images/'
