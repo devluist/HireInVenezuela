08d8466b60ac21d05092b7d3a7227e087ac06f5c
desLT <des.luistena@hotmail.com>
2016-01-18 21:58:43 -0430

    Primera vez en la web - Parte 9

--- wsgi/hv/hv/settings.py
+++ wsgi/hv/hv/settings.py
@@ -112,8 +112,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/multimedia/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'Multimedia/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Additional locations of static files
 STATICFILES_DIRS = (
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
@@ -126,10 +126,10 @@ STATICFILES_DIRS = (
 ##################################
 ##################################
 
-STATICFILES_FINDERS = (
- 	"django.contrib.staticfiles.finders.FileSystemFinder",
-	"django.contrib.staticfiles.finders.AppDirectoriesFinder",
-)
+# STATICFILES_FINDERS = (
+#  	"django.contrib.staticfiles.finders.FileSystemFinder",
+# 	"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# )
 
 
 
