e55fcdc104a7e6ad950245a9a3ef0c3389a13706
desLT <des.luistena@hotmail.com>
2016-01-30 18:18:14 -0430

    Tercera vez en la web - Parte Final

--- wsgi/hv/hv/settings.py
+++ wsgi/hv/hv/settings.py
@@ -194,7 +194,7 @@ ADMIN_MEDIA_PREFIX = '/multimedia/admin/'
 if desarrollando:
 	EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-	EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+	EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 	EMAIL_USE_TLS = True
 	EMAIL_HOST = 'smtp-mail.outlook.com'
 	EMAIL_HOST_USER = 'des.luistena@outlook.com'
