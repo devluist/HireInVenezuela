047454c6cc46905f0d1201cc031def483b531e66
desLT <des.luistena@hotmail.com>
2016-02-03 12:17:17 -0430

    Tercera vez en la web - El Fin

--- wsgi/hv/hv/urls.py
+++ wsgi/hv/hv/urls.py
@@ -45,7 +45,7 @@ urlpatterns = patterns('geoservicios.views',
 	url('^discusion/(?P<thread_id>[\d]+)/(?P<obj_cola>[\d]+)/(?P<msj>[\d]+)/$', ConversationView.as_view(template_name='postman/discusion.html'), name='view_conversation'),
 	url('^mensajeria/', include('postman.urls', namespace='postman', app_name='postman')),
 
-	url('^admin/', include(admin.site.urls)),
+	url('^quien-eres/', include(admin.site.urls)),
 	url('^googlea6116be28450736e.html/$', TemplateView.as_view(template_name="googlea6116be28450736e.html")),
 	url('^robots.txt/$', TemplateView.as_view(template_name="robots.txt")),
 	url('^sitemap.xml/$', TemplateView.as_view(template_name="sitemap.xml")),
